da1013590b04ad08add8938b1abf80908334e162
aamaulana10 <aamaulanabola10@gmail.com>
2021-03-25 16:03:05 +0700

    Add test for travis build

---
+++ AresaTests/AresaTests.swift
@@ -0,0 +1,17 @@
+//
+//  AresaTests.swift
+//  AresaTests
+//
+//  Created by aamaulana10 on 25/03/21.
+//
+
+import XCTest
+
+class AresaTests: XCTestCase {
+
+  func testForTravis() {
+    
+    //
+  }
+
+}
